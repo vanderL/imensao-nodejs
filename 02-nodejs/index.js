666a39afeb757d2088b8a83cda0b18960ec1e036
Vander Lima <vnd.vander@gmail.com>
2020-11-10 16:17:21 -0300

    Code Refactoring - Callback for Promises

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -1,22 +1,33 @@
-function getUser(callback){
-    setTimeout(function (){
-        return  callback(null, {
-            id: 1,
-            nome: 'Aladin',
-            dataNascimento: new Date()
-        })
+const util = require('util')
+const getAddressAsync = util.promisify(getAddress)
 
-    }, 1000)
+function getUser() {
+    //quando der algum problema -> reject(ERRO)
+    //quando sucess -> RESOLV
+    return new Promise(function resolvePromise(resolve, reject){
+        setTimeout(function () {
+            //return reject(new Error('DEU MUITO RUIM'))
+            return resolve({
+                id: 1,
+                nome: 'Aladin',
+                date: new Date()
+            })
+        }, 1000)
+    })
 }
 
-function getPhone(idUsuario, callback) {
-    setTimeout(() => {
-        return callback(null, {
-            phone: '981214026',
-            ddd: 85
 
-        })
-    }, 2000)
+function getPhone(idUsuario) {
+    return new Promise(function resolvePromise(resolve, reject){
+        setTimeout(() => {
+            return resolve({
+                phone: '981214026',
+                ddd: 85
+    
+            })
+        }, 2000)
+
+    })
 }
 
 function getAddress(idUsuario, callback) {
@@ -30,11 +41,45 @@ function getAddress(idUsuario, callback) {
 }
 
 
-function handleUser(erro, user) {
-    console.log('user', user)
-}
 
-getUser(function handleUser(error, user) {
+const userPromise = getUser()
+// para manipular o sucesso usamos a função .then
+// para manipular erros, usamos o .cath
+// user -> telefone -> telefone
+userPromise
+    .then(function(user) {
+        return getPhone(user.id)
+            .then(function resolverPhone(result) {
+                return {
+                    user: {
+                        nome: user.nome,
+                        id: user.id
+
+                    },
+                    phone: result
+                }
+        })
+    })
+    .then(function(resultado){
+        const address = getAddressAsync(resultado.user.id)
+        return address.then(function resolverAddress(result) {
+            return {
+                usuario: resultado.user,
+                phone: resultado.phone,
+                address: result
+            }
+        })
+    })
+    .then(function(result) {
+        console.log(`
+            Nome: ${result.usuario.nome}
+            Endereco: ${result.address.street}, ${result.address.number}
+            Telefone: (${result.phone.ddd}) ${result.phone.phone}`)
+})
+    .catch(function(error){
+        console.error('DEU RUIM', error)
+})
+/*getUser(function handleUser(error, user) {
     //null || "" || 0 === false
     if(error) {
         console.error('DEU RUIM em USUARIO', error)
@@ -60,6 +105,7 @@ getUser(function handleUser(error, user) {
         })
     })
 })
-//const phone = getPhone(user.id)
+
+*///const phone = getPhone(user.id)
 
 //const address = getAddress(user.id)
