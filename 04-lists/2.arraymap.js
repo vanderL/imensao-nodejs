c7d5c1b064b6ef8dea0eff7647c974692ae6c1f4
vanderL <vnd.vander@gmail.com>
2021-02-04 22:07:03 -0300

    Creating my Array.map

--- 04-lists/2.arraymap.js
+++ 04-lists/2.arraymap.js
@@ -1,18 +1,31 @@
 const service = require('./service')
 
-async function main () {
-    const novoArray = []
-    for(let indice = 1; novoArray >  1; indice++) {
-        
+Array.prototype.myMap = function(callback) {
+    const newArray = []
+    for (let index = 0; index < this.length - 1; index++) {
+        const result = callback(this[index], index)
+        newArray.push(result)
     }
+    return newArray
+}
 
+
+async function main () {
     try {
         const results = await service.getPeople(`a`)
-        const names = []
-        results.results.forEach(function (item) {
-            names.push(item.name)
-        })
-        console.log('names', names)
+        //const names = []
+        //results.results.forEach(function (item) {
+        //    names.push(item.name)
+       // })
+
+       //const names = results.results.map(function (people) { 
+         //  return people.name
+       // })
+       
+       const names = results.results.myMap(function(people, index) {
+           return `[${index}]${people.name}`
+       })
+       console.log('names', names)
         
     } catch (error) {
         console.error(`Bad call`, error)
