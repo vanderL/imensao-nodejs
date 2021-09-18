a3621aee7b648582c2bfd1b85906c6d6a4937132
vanderL <vnd.vander@gmail.com>
2021-09-18 19:45:00 -0300

    Array.prototype filter

--- 04-lists/3-filter.js
+++ 04-lists/3-filter.js
@@ -1,8 +1,40 @@
 const { getPeople } =  require('./service');
 
+//implementação propria
+Array.prototype.myFilter = function (callback) {
+    const list = []
+    for(index in this) {
+        const item = this[index];
+        const result = callback(item, index, this)
+
+        if (!result) continue;
+        list.push(item)
+    }
+
+    return list;
+}
+
+
+
+
 async function main(){
     try {
-        
+        const { results } = await getPeople( `a `)
+
+        // const familiaLars = results.filter(function (item) {
+            
+        //     const result = item.name.toLowerCase().indexOf(`lars`) !== -1
+        //     return result
+        // })
+
+        const familiaLars = results.myFilter((item, index, lista) => {
+            console.log(`index: ${index}`, lista.length)
+            return item.name.toLowerCase().indexOf('lars') !== -1
+        })
+
+        const names = familiaLars.map((pessoa) => pessoa.name)
+        console.log(names)
+
     } catch (error) {
         console.log('error', error);
     }
